c3d700ae32470d993943cbdcb3ddd9e9df65ceb7
Vidhi Makani <100896986+vidhimakani74@users.noreply.github.com>
2022-12-05 16:40:28 +0530

    edge case added and loss msg updated

--- app.js
+++ app.js
@@ -1,34 +1,40 @@
-var initialPrice=document.querySelector("#initial-price");
-var qauntity=document.querySelector("#qty");
-var currentaPrice=document.querySelector("#current-price");
+var initialPrice = document.querySelector("#initial-price");
+var qauntity = document.querySelector("#qty");
+var currentaPrice = document.querySelector("#current-price");
 var btn = document.querySelector("#check-button");
-var output=document.querySelector("#output");
+var output = document.querySelector("#output");
 
-function calculateProfitAndLoss(ip,qp,cp){
-    if(ip>cp){
-        var loss=(ip-cp)*qp;
-        var lossPercentage=(loss/ip)*100;
-        showOutput(`your profit is ${loss} and your profit-percentage is ${lossPercentage}% 🙁`); 
+function calculateProfitAndLoss(ip, qp, cp) {
+    if (ip > 0 && qp > 0 && cp > 0) {
+        if (ip > cp) {
+            var loss = (ip - cp) * qp;
+            var lossPercentage = (loss / ip) * 100;
+            showOutput(`your loss is ${loss} and your loss-percentage is ${lossPercentage}% 🙁`);
+        }
+        else if (cp > ip) {
+            var profit = (cp - ip) * qp;
+            var profitPercentage = (profit / ip) * 100;
+            showOutput(`your profit is ${profit} and your profit-percentage is ${profitPercentage}% 🤑`);
+        }
+        else {
+            showOutput("No pain no gain , no gain no pain 😶");
+        }
     }
-    else if(cp>ip){
-        var profit = (cp-ip)*qp;
-        var profitPercentage= (profit/ip)*100;
-        showOutput(`your profit is ${profit} and your profit-percentage is ${profitPercentage}% 🤑`);    
-     }   
-     else{
-        showOutput("No pain no gain , no gain no pain 😶");   
-     }
+    else {
+        showOutput("Please enter only positive number!📢");
+    }
 }
 
-function calculate(){
+function calculate() {
     var ip = Number(initialPrice.value);
     var qp = Number(qauntity.value);
     var cp = Number(currentaPrice.value);
-    calculateProfitAndLoss(ip,qp,cp)
+    calculateProfitAndLoss(ip, qp, cp)
 }
 
-function showOutput(msg){
-    output.innerText=msg;
+function showOutput(msg) {
+    output.innerText = msg;
 }
+
 btn.addEventListener("click", calculate);
 
